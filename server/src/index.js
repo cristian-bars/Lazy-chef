4c754772b006fd920f34c225b6df1893c1d62c9a
cristian-bars <patxi.cristian@gmail.com>
2021-06-01 12:06:36 +0200

    Add autorization and autentication strategies

--- server/src/index.js
+++ server/src/index.js
@@ -2,30 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const chalk = require('chalk');
 const debug = require('debug')('app');
+const passport = require('passport');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth.routes');
 require('dotenv').config();
+require('./passport/passport.config');
+require('./ddbb/mongoose.config');
 
 const port = process.env.PORT;
 const server = express();
 
-mongoose.connect(
-  process.env.DDBB_URL,
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  }
-);
-
 server.use(cors());
 server.use(express.json());
 server.use(morgan('dev'));
+server.set('view engine', 'ejs');
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 
 const recipesRouter = require('./routes/recipesRouter');
 const usersRouter = require('./routes/usersRouter');
 
 server.use('/recipes', recipesRouter);
 server.use('/users', usersRouter);
+server.use('/', authRoutes);
+server.use(
+  '/users',
+  passport.authenticate('jwt', { session: false }),
+  authRoutes
+);
 
 server.listen(port,
   () => debug(`Server is running in ${chalk.yellow(`localhost:${port}`)}`));
