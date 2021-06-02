ee4c5a6a14dcba3e5414cdd4faa48659380748a9
cristian-bars <patxi.cristian@gmail.com>
2021-06-02 16:29:08 +0200

    Create redux and connect with database

--- client/lazyChef/App.js
+++ client/lazyChef/App.js
@@ -1,21 +1,26 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Provider} from 'react-redux';
 import Dashboard from './src/components/dashboard/Dashboard';
 import Login from './src/components/login/Login';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import Home from './src/components/home/Home';
+import store from './src/redux/stores/index';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Dashboard} />
-        <Stack.Screen name="Register" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store()}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+          <Stack.Screen name="Register" component={Login} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
---
+++ client/lazyChef/src/redux/actions/actionCreators.js
@@ -0,0 +1,62 @@
+import actionTypes from './actionTypes';
+import axios from 'axios';
+
+//const url = 'http://localhost/recipes';
+const url = 'http://192.168.0.29:2022/recipes';
+
+export function loadRecipes() {
+  return async dispatch => {
+    try {
+      const {data} = await axios(url);
+      dispatch({
+        type: actionTypes.LOAD_RECIPES,
+        recipes: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: 'LOAD_RECIPES_ERROR',
+      });
+    }
+  };
+}
+
+export function addRecipe(recipe) {
+  return {
+    type: actionTypes.ADD_RECIPE,
+    recipe,
+  };
+}
+
+export function deleteRecipe(recipeId) {
+  return async dispatch => {
+    await axios.delete(`${url}/${recipeId.id}`);
+    dispatch({
+      type: actionTypes.DELETE_RECIPE,
+      recipeId,
+    });
+  };
+}
+
+export function updateRecipe(recipe) {
+  return {
+    type: actionTypes.UPDATE_RECIPE,
+    recipe,
+  };
+}
+
+export function loadRecipe(recipe) {
+  return {
+    type: actionTypes.LOAD_RECIPE,
+    recipe,
+  };
+}
+
+export function getRecipeById(recipeId) {
+  return async dispatch => {
+    const {data} = await axios(`${url}/${recipeId}`);
+    dispatch({
+      type: actionTypes.LOAD_RECIPE,
+      recipe: data,
+    });
+  };
+}
