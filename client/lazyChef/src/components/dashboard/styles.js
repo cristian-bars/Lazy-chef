2ff57b077bd406b99afd5bfd9b5b7aae685362c6
cristian-bars <patxi.cristian@gmail.com>
2021-06-02 14:45:28 +0200

    Create navigation and styles files

---
+++ client/lazyChef/src/components/dashboard/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 450,
+    height: 450,
+  },
+  register: {
+    color: 'white',
+  },
+  touchRegister: {
+    width: 300,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default styles;
---
+++ client/lazyChef/src/components/login/styles.js
@@ -0,0 +1,30 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#044AFD',
+    alignItems: 'center',
+  },
+  formLogin: {
+    width: 300,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    width: 300,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 100,
+  },
+});
+
+export default styles;
